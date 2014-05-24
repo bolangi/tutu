8c4b63432c6a62a82e77013e4923d0898db989ca
Joel Roth <joelz@pobox.com>
2014-05-24 12:43:01 -1000

    copy sample directory before modifying replacement

--- customize.pl
+++ customize.pl
@@ -3,6 +3,37 @@ use Modern::Perl;
 use Data::Dumper::Concise;
 
 my @customize = split "\n",qx(cat ./MY_WEBSITE_INIT);
+my %replace;
+my $rep;
+for (@customize)
+{
+	chomp;
+	my ($var, $replacement) = split " ", $_, 2;
+	$var =~ s/\s+$//;		
+	$replacement =~ s/\s+$//;		
+	$replacement =~ s{([\@\$])}{\\$1}g;
+
+	$replace{$var} = $replacement;
+	
+	$var =~ s{\$}{\\\$};
+	$rep .= "s{$var}{$replacement}; ";
+}
+
+my $copy_domain_dir_cmd = "cp -a example.com $replace{'$WEBSITE_DOMAIN'}";
+say "
+copy domain directory: 
+    $copy_domain_dir_cmd
+";
+system $copy_domain_dir_cmd;
+
+my $copy_webapp_dir_cmd = "cp -a webapp $replace{'$WEBSITE_ROOT'}";
+say "
+copy webapp directory: 
+    $copy_webapp_dir_cmd
+";
+system $copy_webapp_dir_cmd;
+
+
 my $grep = q(grep -lrP '\$[A-Z]' example.com webroot);
 my $targets = join " ",map{chomp; $_} qx($grep);
 say "
@@ -16,20 +47,6 @@ say "Found targets:
 
 # escape sigils
 
-my $rep;
-for (@customize)
-{
-	chomp;
-	my ($var, $replacement) = split " ", $_, 2;
-	$var =~ s/\s+$//;		
-	$replacement =~ s/\s+$//;		
-
-	$replacement =~ s{([\@\$])}{\\$1}g;
-	
-	
-	$var =~ s{\$}{\\\$};
-	$rep .= "s{$var}{$replacement}; ";
-}
 
 #say Dumper \%replace;
 $rep =~ s{'}{&#39;}g;
