c33d1f97f00d236200ca9e30a01ca44513223503
Joel Roth <joelz@pobox.com>
2014-05-24 10:53:27 -1000

    customize

--- customize.pl
+++ customize.pl
@@ -3,10 +3,10 @@ use Modern::Perl;
 use Data::Dumper::Concise;
 
 my @customize = split "\n",<<'END';
-$WEBROOT /home/maxfankel/spacesystems
+$WEBROOT /home/maxfankel/space
 $WEBSITE_DOMAIN maxfankel.com
 $WEBSITE_URL http://maxfankel.com
-$WEBSITE_NAME Max Fankel Space Systems Website
+$WEBSITE_NAME Fankel's Space Systems
 $WEBMASTER_NAME Max Fankel
 $WEBMASTER_MAIL_ADDRESS me@maxfankel.com
 END
