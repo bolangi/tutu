75534806cbedf95a3a71993b654962d6d37d2038
Joel Roth <joelz@pobox.com>
2014-05-24 13:06:03 -1000

    customize

--- customize.pl
+++ customize.pl
@@ -19,30 +19,28 @@ for (@customize)
 	$rep .= "s{$var}{$replacement}; ";
 }
 
-my $new_domain_dir = $replace{'maxfankel.com'};
+my $new_domain_dir = $replace{'$WEBSITE_DOMAIN'};
+my ($new_webapp_dir) = $replace{'$WEBSITE_ROOT'} =~ m{/?([^/]+)/?$};
+
+say ("$new_domain_dir or $new_webapp_dir already exist.
+Will not overwrite. Fix this and run the script again."), exit
+	if -d $new_domain_dir or -d $new_webapp_dir;
 my $copy_domain_dir_cmd = "cp -a example.com $new_domain_dir";
 say "
 copying domain directory: 
     $copy_domain_dir_cmd
 ";
--d $new_domain_dir 
-	and say ("directory $new_domain_dir already exists, fix this and try again."),
-	exit
-or system $copy_domain_dir_cmd;
+system $copy_domain_dir_cmd;
 
-my ($new_webapp_dir) = $replace{'/home/maxfankel/space'} =~ m{/?([^/]+)/?$};
 
 my $copy_webapp_dir_cmd = "cp -a webapp $new_webapp_dir";
 say "
 copying webapp directory: 
     $copy_webapp_dir_cmd
 ";
--d $new_webapp_dir 
-	and say ("directory $new_webapp_dir already exists, fix this and try again."),
-	exit
-or system $copy_webapp_dir_cmd;
+system $copy_webapp_dir_cmd;
 
-my $grep = q(grep -lrP '\$[A-Z]'  $new_webapp_dir);
+my $grep = qq(grep -lrP '\\\$[A-Z]'  $new_domain_dir $new_webapp_dir);
 my $targets = join " ",map{chomp; $_} qx($grep);
 say "
 Searching for files using shell command: 
@@ -53,12 +51,8 @@ say "Found targets:
 
     $targets";
 
-# escape sigils
-
-
-#say Dumper \%replace;
 $rep =~ s{'}{&#39;}g;
-my $command = qq(perl -p -i.bak -E '$rep' $targets);
-say;
+my $command = qq(perl -p -i -E '$rep' $targets);
+say "shell command";
 say $command;
 `$command`;
